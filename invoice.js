033c59c6db688fa8bab938d2f8c1a5a15761babd
ou75189-netizen <ou75189@students.cavendish.ac.ug>
2026-05-17 12:56:27 +0300

    Create invoice.js

---
+++ invoice.js
@@ -0,0 +1,55 @@
+document.getElementById("invoiceNumber")
+.innerText = generateInvoiceNumber();
+
+function addItem(){
+
+  let table =
+    document.getElementById("invoiceItems");
+
+  let row = document.createElement("tr");
+
+  row.classList.add("item-row");
+
+  row.innerHTML = `
+
+    <td>
+      <input type="text"
+      placeholder="Description">
+    </td>
+
+    <td>
+      <input type="number"
+      class="qty"
+      value="1"
+      oninput="calculateTotals()">
+    </td>
+
+    <td>
+      <input type="number"
+      class="rate"
+      value="0"
+      oninput="calculateTotals()">
+    </td>
+
+    <td class="row-total">
+      0
+    </td>
+
+    <td>
+      <button onclick="removeItem(this)">
+        Delete
+      </button>
+    </td>
+  `;
+
+  table.appendChild(row);
+
+  calculateTotals();
+}
+
+function removeItem(button){
+  button.parentElement.parentElement.remove();
+  calculateTotals();
+}
+
+addItem();
