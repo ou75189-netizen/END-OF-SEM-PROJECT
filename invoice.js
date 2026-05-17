9454b398287182629b9333a19285c8a6b2e7f146
ou75189-netizen <ou75189@students.cavendish.ac.ug>
2026-05-17 13:38:40 +0300

    Implement loader display functions
    Add functions to show and hide a loading indicator.

--- invoice.js
+++ invoice.js
@@ -66,3 +66,14 @@ if(editingInvoice){
 
   localStorage.removeItem("editingInvoice");
 }
+function showLoader(){
+
+  document.getElementById("loader")
+  .style.display = "block";
+}
+
+function hideLoader(){
+
+  document.getElementById("loader")
+  .style.display = "none";
+}
